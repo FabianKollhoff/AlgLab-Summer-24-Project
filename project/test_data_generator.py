55314bb4403d37748d67d8bb8bea391c0e1100e3
Fynn Gläser <f.glaeser@tu-braunschweig.de>
2024-06-14 21:13:01 +0200

    implemented average score distribution for projects, a little unefficient

--- project/test_data_generator.py
+++ project/test_data_generator.py
@@ -1,6 +1,8 @@
 from data_schema import Project, Student, Instance
 import json
 import csv
+import numpy as np
+from ortools.sat.python import cp_model
 
 import random
 
@@ -12,41 +14,32 @@ class Generator():
         distribution = (0.2,0.1,0.4,0.1)
         if rating <= distribution[0]:
             # cracked student
-            project_distribution = (0.4, 0.1, 0.1, 0,1)
             skills = (2, 2, 2, 2, 2)
         if rating <= distribution[0] + distribution[1]:
             # basic student
-            project_distribution = (0.3, 0.1, 0.3, 0,1)
             skills = (4, 2, 2, 3, 4)
         if rating <= distribution[0] + distribution[1]+ distribution[2]:
             # python bro
-            project_distribution = (0.3, 0.1, 0.1, 0,4)
             skills = (2, 3, 3, 3, 4)
         if rating <= distribution[0] + distribution[1] + distribution[2] + distribution[3]:
             # web developer
-            project_distribution = (0.3, 0.2, 0.1, 0,4)
             skills = (4, 3, 3, 2, 2)
         if rating > distribution[0] + distribution[1] + distribution[2] + distribution[3]:
             # got here by copying homework
-            project_distribution = (0.4, 0.2, 0.1, 0,1)
             skills = (4, 4, 4, 4, 4)
-        return project_distribution, skills
+        return skills
     
-    def randomStudentRankingProject(self, project_id, num_students, percent_fav_projects):
-        bonus = 0
-        if project_id <= int(num_students / percent_fav_projects):
-            bonus = 0.5
+    def randomStudentRankingProject(self, project_distribution):
         rating = random.random()
-        project_distribution, skills = self.randomStudentType()
-        if rating <= project_distribution[0] + bonus:
-            return 5
+        if rating <= project_distribution[0]:
+            return 1
         if rating <= project_distribution[0] + project_distribution[1]:
-            return 4
+            return 2
         if rating <= project_distribution[0] + project_distribution[1]+ project_distribution[2]:
             return 3
         if rating <= project_distribution[0] + project_distribution[1] + project_distribution[2] + project_distribution[3]:
-            return 2
-        return 1
+            return 4
+        return 5
     
 
     def randomProjectCapacity(self):
@@ -67,11 +60,12 @@ class Generator():
         return projects
 
 
-    def generateProjectsRatings(self, percent_fav_projects):
-        return {project : self.randomStudentRankingProject(project, number_students, percent_fav_projects) for project in self.projects}
+    def generateProjectsRatings(self):
+        rating_probalilitys = self.generateDistrubution(self.projects)
+        return {project : self.randomStudentRankingProject(rating_probalilitys[project]) for project in self.projects}
     
     def generateSkillRatings(self):
-        data, skills = self.randomStudentType()
+        skills = self.randomStudentType()
         skill_dict = {"Python": skills[0]}
         skill_dict.update({"Java": skills[1]})
         skill_dict.update({"C++": skills[2]})
@@ -79,17 +73,52 @@ class Generator():
         skill_dict.update({"PHP": skills[4]})
         return skill_dict
 
-    def generateStudents(self, number_students, percent_fav_projects):
+    def generateStudents(self, number_students):
         students = []
         for i in range(number_students):
-            student = Student(last_name="Doe", first_name="Joe", matr_number=i, projects_ratings=self.generateProjectsRatings(percent_fav_projects), skills_ratings=self.generateSkillRatings())
+            student = Student(last_name="Doe", first_name="Joe", matr_number=i, projects_ratings=self.generateProjectsRatings(), skills_ratings=self.generateSkillRatings())
             students.append(student)
         return students
-
-    def generateInstance(self, number_projects, number_students, percent_fav_projects):
+    
+    def generateDistrubution(self, projects):
+        number_projects = len(projects)
+        mu = 3.5
+        sigma = int(len(projects)/10)
+        # generate a average rating distribution in form of a gaussian distribution
+        average_ratings = np.random.normal(mu, sigma, number_projects)
+        # clamp ratings to be between 1 and 5
+        average_ratings = np.clip(average_ratings, 1, 5)
+        return {project: self.calculateRatingProbabilities(rating) for project,rating in zip(projects,average_ratings)}
+    
+    def calculateRatingProbabilities(self, average_rating):
+        model = cp_model.CpModel()
+        # define variables for probabilities, elevate the upper bound because we are working with integers
+        p1 = model.NewIntVar(0, 100, 'p1')
+        p2 = model.NewIntVar(0, 100, 'p2')
+        p3 = model.NewIntVar(0, 100, 'p3')
+        p4 = model.NewIntVar(0, 100, 'p4')
+        p5 = model.NewIntVar(0, 100, 'p5')
+        # ensure that probabilities add up to 1
+        model.Add(p1 + p2 + p3 + p4 + p5 == 100)
+        # minimize distance between sum of weighted probabilities and average_rating
+        difference = model.NewIntVar(-10000, 10000, 'difference')
+        abs_difference = model.NewIntVar(0, 10000, 'abs_difference')
+        model.Add(difference == (1*p1 + 2*p2 + 3*p3 + 4*p4 + 5*p5) - int(average_rating*100))
+        model.AddAbsEquality(abs_difference, difference)
+        model.Minimize(abs_difference)
+        # solve model, return prababilities as a list
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
+        probabilities = []
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+            total = solver.Value(p1) + solver.Value(p2) + solver.Value(p3) + solver.Value(p4) + solver.Value(p5)
+            probabilities = [solver.Value(p1)/total,solver.Value(p2)/total, solver.Value(p3)/total, solver.Value(p4)/total, solver.Value(p5)/total]
+        return probabilities
+        
+    def generateInstance(self, number_projects, number_students):
         self.sumProjectsCapacity = 0
         self.projects = self.generateProjects(number_projects=number_projects, number_students=number_students)
-        self.students = self.generateStudents(number_students=number_students, percent_fav_projects=percent_fav_projects)
+        self.students = self.generateStudents(number_students=number_students)
         self.instance = Instance(
                 students=self.students,
                 projects=self.projects
@@ -105,7 +134,7 @@ instance_sizes = [(10,100),(20,200), (30, 300), (50,500), (100,1000)]
 
 for instance_size in instance_sizes:
     number_projects, number_students = instance_size
-    data = g.generateInstance(number_students = number_students,number_projects = number_projects, percent_fav_projects=10).model_dump_json()
+    data = g.generateInstance(number_students = number_students,number_projects = number_projects).model_dump_json()
     with open(f"instances/data_s{number_students}_g{number_projects}.json", 'w') as f:
         f.write(data)
 
