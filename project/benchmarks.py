cfb0208c900c436b90052814604d7f760945f308
felix <f.wischhusen@tu-braunschweig.de>
2024-06-25 12:49:49 +0200

    first attempt to visualize programming requirements in solution

--- project/benchmarks.py
+++ project/benchmarks.py
@@ -22,11 +22,12 @@ class Benchmarks(BaseModel):
 
         # self.log_avg_rating()
 
-        self.log_rating_sums()
+        # self.log_rating_sums()
         # TODO: log the number of partner requests that were fulfilled
 
         # TODO: log graph of friend relations that were (not) fulfilled
-        self.log_friend_graph()
+        #self.log_friend_graph()
+        self.log_programming_requirements()
 
     def log_rating_sums(self):
         # plot bar chart for student ratings in solution
@@ -142,6 +143,29 @@ class Benchmarks(BaseModel):
         plt.title(f"Friend graph. G:{num_greens} R:{num_reds}")
         plt.show()
         return plt.gcf()
+    
+    def log_programming_requirements(self):
+        # in solution: FOr each project log for each programming language if requirement was met
+        # pro language 2 bar charts. Links requirement für language, rechts anzahl students mit skill > 1 in lang
+        # pro lang: Prozentuale auslastung des requirements
+        languages = [lang for lang in self.instance.students[0].programming_language_ratings.keys()]
+        for proj in self.solution.projects:
+            #get list of all requirements
+            reqs = []
+            num_students_with_skill = []
+
+            ax  = plt.subplot()
+            for lang, req in self.instance.projects[proj].programming_requirements.items():
+                # get number of students with language skill for this lang
+                num_stu_with_skill = sum(1 for stu in self.solution.projects[proj] if stu.programming_language_ratings[lang] > 3)
+                reqs.append(req)
+                num_students_with_skill.append(num_stu_with_skill)
+            x = list(range(len(languages)))
+            ax.bar([el - 0.2 for el in x], reqs, width=0.4, color='b', align='center')
+            ax.bar([el + 0.2 for el in x], num_students_with_skill, width=0.4, color='r', align='center')
+            ax.set_title("Programming requirements for Project. B=Required, R=Num students")
+            plt.xticks(x, languages)
+            plt.show()
 
 
 if __name__ == "__main__":
--- project/verify.py
+++ project/verify.py
@@ -1,6 +1,7 @@
 from _alglab_utils import CHECK, main, mandatory_testcase
 from data_schema import Instance
 from solver import SepSolver
+from benchmarks import Benchmarks
 
 
 def solve_sep_instance(filepath: str):
@@ -42,7 +43,8 @@ def solve_sep_instance(filepath: str):
             count_student_in_solution == 1,
             f"The returned solution contains a student {student_instance} {count_student_in_solution} times!",
         )
-
+    benchmark = Benchmarks(solution=solution, instance=instance)
+    benchmark.log()
     return instance, solution
 
 
