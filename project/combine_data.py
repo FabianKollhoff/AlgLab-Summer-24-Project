54334e651b735056f00f96bc7d7bc1f976c57fdf
Fynn Gläser <f.glaeser@tu-braunschweig.de>
2024-07-06 23:26:44 +0200

    added construction and combining of project data json files

--- project/combine_data.py
+++ project/combine_data.py
@@ -11,8 +11,21 @@ for filename in os.listdir(directory):
         with open(filepath, 'r') as file:
             student_data = json.load(file)
             students.append(student_data)
-
+            
 combined_data = {"students": students}
+            
+directory = 'project/instances/projects'
+
+projects = []
+            
+for filename in os.listdir(directory):
+    if filename.endswith('.json'):
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'r') as file:
+            project_data = json.load(file)
+            projects.append(project_data)
+
+combined_data.update({"projects": projects})
 
 with open('project/instances/SEP_data.json', 'w') as outfile:
     json.dump(combined_data, outfile, indent=4)
