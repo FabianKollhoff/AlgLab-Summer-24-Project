a35a1a021bd8821f6c4bd9bbd0e4be4ddc856020
Fabian Kollhoff <f.kollhoff@tu-bs.de>
2024-07-09 20:31:59 +0200

    infeasble solution

--- project/sep_configurator.py
+++ project/sep_configurator.py
@@ -39,14 +39,17 @@ def solve_instance(num):
     solution = verify.solve_next_objective(solver=solver,instance=instance)
     num.value = 0.25
     if solution == None:
+        num.value = -1
         return solution, instance
     solution = verify.solve_next_objective(solver=solver,instance=instance)
     num.value = 0.5
     if solution == None:
+        num.value = -1
         return solution, instance
     solution = verify.solve_next_objective(solver=solver,instance=instance)
     num.value = 0.75
     if solution == None:
+        num.value = -1
         return solution, instance
     solution = verify.solve_next_objective(solver=solver,instance=instance)
     num.value = 1
@@ -81,6 +84,7 @@ if authentication_status:
             time.sleep(0.1)
             end = time.time_ns()
             progress_text = ""
+
             if num.value == 0:
                 progress_text = "project rating objective"      #TODO: add optimal group size objective
             elif num.value == 0.25:
@@ -94,32 +98,35 @@ if authentication_status:
             timer.metric("Elapsed time:", F"{round((end-start)/1000000000, 3)}")
             my_bar.progress(num.value, text=progress_text)
         
-        with open("solution/solution_of_100_1000.json") as f:
-        #with open("solution/solution_of_sep.json") as f:
-            solution: Solution = Solution.model_validate_json(f.read())
+        if num.value == -1:
+            st.write("!!! INFEASABLE SOLUTION !!!")
+        else:
+            with open("solution/solution_of_100_1000.json") as f:
+            #with open("solution/solution_of_sep.json") as f:
+                solution: Solution = Solution.model_validate_json(f.read())
 
-        with open("./instances/data_s1000_g100.json") as f:
-        #with open("./instances/SEP_data.json") as f:
-            instance: Instance = Instance.model_validate_json(f.read())
-        benchmark = Benchmarks(solution=solution, instance=instance)
+            with open("./instances/data_s1000_g100.json") as f:
+            #with open("./instances/SEP_data.json") as f:
+                instance: Instance = Instance.model_validate_json(f.read())
+            benchmark = Benchmarks(solution=solution, instance=instance)
 
-        st.write("""average ratings project""")
+            st.write("""average ratings project""")
 
-        y_rating, x_rating = benchmark.log_rating_sums()
-        chart_data = pd.DataFrame(x_rating, y_rating)
-        st.bar_chart(chart_data)
+            y_rating, x_rating = benchmark.log_rating_sums()
+            chart_data = pd.DataFrame(x_rating, y_rating)
+            st.bar_chart(chart_data)
 
-        st.write("""percentage programming requirements""")
+            st.write("""percentage programming requirements""")
 
-        x,y = benchmark.log_programming_requirements()
-        chart_data = pd.DataFrame(y, x)
-        st.bar_chart(chart_data)
+            x,y = benchmark.log_programming_requirements()
+            chart_data = pd.DataFrame(y, x)
+            st.bar_chart(chart_data)
 
-        st.write("""project utilization""")
+            st.write("""project utilization""")
 
-        x,y = benchmark.log_proj_util()
-        chart_data = pd.DataFrame(y, x)
-        st.bar_chart(chart_data)
+            x,y = benchmark.log_proj_util()
+            chart_data = pd.DataFrame(y, x)
+            st.bar_chart(chart_data)
 
 
 elif authentication_status is False:
